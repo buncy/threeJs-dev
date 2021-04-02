0a17776bdd40a2feaa82b2d006766c1a3d0fa3ad
buncy <karthik138105@gmail.com>
2021-04-02 10:38:11 +0530

    lights

--- src/script.js
+++ src/script.js
@@ -1,6 +1,8 @@
 import intro from "./Basics/intro";
 import materials from "./Basics/materials";
 import text_3d from "./Basics/3d-text";
+import lights from "./Basics/lights";
 //intro();
 //materials();
-text_3d();
+// text_3d();
+lights();
