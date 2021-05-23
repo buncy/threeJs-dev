1f681fbfba1ccd7d4b00ab9dd04d2308c0b23edf
buncy <karthik138105@gmail.com>
2021-05-23 18:52:52 +0530

    raycaster

--- src/script.js
+++ src/script.js
@@ -4,9 +4,12 @@ import text_3d from "./Basics/3d-text";
 import lights from "./Basics/lights";
 import hauntedHouse from "./Basics/haunted-house";
 import particles from "./Basics/particles";
+import raycaster from "./Basics/raycaster";
+
 //intro();
 //materials();
 // text_3d();
 //lights();
 //hauntedHouse();
-particles();
+//particles();
+raycaster()
